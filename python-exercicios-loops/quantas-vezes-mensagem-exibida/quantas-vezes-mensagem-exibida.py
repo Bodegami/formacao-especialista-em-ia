33453a731737cc448e0f59f24ac978b4fc4c1af0
Renato Ferreira <rena.f.oliveira@gmail.com>
2026-05-30 16:45:08 -0300

    exercicio 1

---
+++ python-exercicios-loops/quantas-vezes-mensagem-exibida/quantas-vezes-mensagem-exibida.py
@@ -0,0 +1,14 @@
+""""
+Quantas vezes a mensagem será exibida?
+
+Marcos está desenvolvendo um programa para exibir uma mensagem de boas-vindas repetidamente no console, 
+como parte de uma campanha de marketing de sua plataforma chamada Buscante. 
+Ele quer garantir que a mensagem seja exibida 5 vezes.
+
+Ajude Marcos a escrever um programa que exiba a mensagem: "Bem-vindo ao Buscante!" o número exato de vezes que ele precisa.
+"""
+
+message = "Bem-vindo ao Buscante!"
+
+for i in range(5):
+    print(message)
