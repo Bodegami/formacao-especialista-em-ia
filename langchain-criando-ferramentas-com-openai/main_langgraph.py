6ca182444439c904e1eb54e0cbb53d750eece142
Renato Ferreira <rena.f.oliveira@gmail.com>
2026-08-22 10:25:27 -0300

    Aula 4 - orquestrando assistentes com LangGraph

--- langchain-criando-ferramentas-com-openai/main_langgraph.py
+++ langchain-criando-ferramentas-com-openai/main_langgraph.py
@@ -1,52 +1,84 @@
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from typing import TypedDict, Literal
 from dotenv import load_dotenv
+from typing import Literal, TypedDict
+from langgraph.graph import StateGraph, START, END
+from langchain_core.runnables import RunnableConfig
+import asyncio
 import os
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 modelo = ChatOpenAI(
-    model_name="gpt-4o-mini",
-    openai_api_key=api_key,
-    temperature=0.5
+    model="gpt-4o-mini",
+    temperature=0.5,
+    api_key=api_key
 )
 
-prompt_consultor_praia = ChatPromptTemplate.from_messages([
-    ("system", "Apresente-se como Sra Praia. Você é uma especialista em viagens com destinos para praias."),
-    ("human", "{query}")
-])
+prompt_consultor_praia = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Apresente-se como Sra Praia. Você é uma especialista em viagens com destinoas para praia."),
+        ("human", "{query}")
+    ] 
+)
 
-prompt_consultor_montanha = ChatPromptTemplate.from_messages([
-    ("system", "Apresente-se como Sra Montanha. Você é uma especialista em viagens com destinos para montanhas e atividades radicais."),
-    ("human", "{query}")
-])
+prompt_consultor_montanha = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Apresente-se como Sr Montanha. Você é uma especialista em viagens com destinoas para montanhas e atividades radicais."),
+        ("human", "{query}")
+    ] 
+)
 
 cadeia_praia = prompt_consultor_praia | modelo | StrOutputParser()
-cadeia_montanha = prompt_consultor_montanha | modelo | StrOutputParser()
+cadeia_montanha = prompt_consultor_montanha |modelo | StrOutputParser()
 
-# O TypedDict é usado para definir a estrutura de dados esperada para a rota, 
-# Usando o Literal, garantindo que o destino seja apenas "praia" ou "montanha".
 class Rota(TypedDict):
     destino: Literal["praia", "montanha"]
 
-prompt_roteador = ChatPromptTemplate.from_messages([
-    ("system", "Responda apenas com 'praia' ou 'montanha' para indicar o tipo de destino que o usuário deseja."),
-    ("human", "{query}")
-])
+prompt_roteador = ChatPromptTemplate.from_messages(
+    [
+        ("system", "Responda apenas com 'praia' ou 'montanha'"),
+        ("human", "{query}")
+    ]
+)
 
-roteador = prompt_roteador | modelo.with_structured_output(Rota) 
+roteador = prompt_roteador | modelo.with_structured_output(Rota)
 
-def response(pergunta: str):
-    rota = roteador.invoke({"query": pergunta})["destino"]
-    print(rota)
-    if rota == "praia":
-        return cadeia_praia.invoke({"query": pergunta})
-    elif rota == "montanha":
-        return cadeia_montanha.invoke({"query": pergunta})
-    else:
-        return "Desculpe, não consegui identificar o tipo de destino desejado. Por favor, tente novamente."
+class Estado(TypedDict):
+    query:str
+    destino: Rota
+    resposta:str
 
-print(response("Quero surfar em um lugar quente."))
+async def no_roteador(estado: Estado, config=RunnableConfig):
+    return {"destino": await roteador.ainvoke({"query":estado["query"]}, config)} 
+
+async def no_praia(estado: Estado, config=RunnableConfig):
+    return {"resposta": await cadeia_praia.ainvoke({"query":estado["query"]}, config)} 
+
+async def no_montanha(estado: Estado, config=RunnableConfig):
+    return {"resposta": await cadeia_montanha.ainvoke({"query":estado["query"]}, config)} 
+
+def escolher_no(estado:Estado)->Literal["praia", "montanha"]:
+    return "praia" if estado["destino"]["destino"] == "praia" else "montanha"
+
+grafo = StateGraph(Estado)
+grafo.add_node("rotear", no_roteador)
+grafo.add_node("praia", no_praia)
+grafo.add_node("montanha", no_montanha)
+
+grafo.add_edge(START, "rotear")
+grafo.add_conditional_edges("rotear", escolher_no)
+grafo.add_edge("praia", END)
+grafo.add_edge("montanha", END)
+
+app = grafo.compile()
+
+async def main():
+    resposta = await app.ainvoke(
+        {"query": "Quero escalar montanhas radicais no sul do Brasil"}
+    )
+    print(resposta["resposta"])
+
+asyncio.run(main())
